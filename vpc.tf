ff41e3d2d21b6a1d725f6e37d18a9dcf052aaa54
aadarsh <aadarsh153@gmail.com>
2025-06-01 23:26:48 +0530

    Terraform

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/aadarshputtamaneni/Terraform_AWS_VPC.git"
+    source = "git::https://github.com/aadarshputtamaneni/Terraform_AWS_VPC.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
